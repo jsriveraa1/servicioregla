23c377720b0784aa6f59dc677c9f52357036be94
jsriveraa1 <juan.sebastian.rivera.abreo@gmail.com>
2022-09-11 18:55:06 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+from flask_restful import Api
+from vistas import VistaReglas
+app = Flask('regla')
+
+
+app_context=app.app_context()
+app_context.push()
+cors = CORS(app)
+api=Api(app)
+api.add_resource(VistaReglas,"/reglas")
